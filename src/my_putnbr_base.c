eaf4f4ff519a4e4130c690b4a174d33360484f18
Raphaël Goulmot <raphael.goulmot@epitech.eu>
2017-04-28 17:26:10 +0200

    Add on github

--- src/my_putnbr_base.c
+++ src/my_putnbr_base.c
@@ -5,7 +5,7 @@
 ** Login   <raphael.goulmot@epitech.net>
 ** 
 ** Started on  Thu Nov 10 19:32:20 2016 Raphaël Goulmot
-** Last update Mon Nov 14 14:37:11 2016 Raphaël Goulmot
+** Last update Sun Mar  5 00:52:11 2017 Raphaël Goulmot
 */
 
 #include "utils.h"
@@ -28,4 +28,29 @@ void	my_putnbr_base(int nbr, char *base)
       my_putchar(base[(nbr / my_power(size, n))]);
       nbr = nbr % my_power(size, n);
     }
+}
+
+
+int	my_getnbr_base(char *str, char *base)
+{
+  int	value;
+  int	i;
+  int	b;
+  char	check;
+
+  value = 0;
+  i = 0;
+  while (str[i])
+    {
+      b = 0;
+      check = 0;
+      while (base[b] && !check && ++b)
+	{
+	  if (base[b - 1] == str[i])
+	    check = 1;
+	}
+      value += b;
+      i++;
+    }
+  return (value);
 }
